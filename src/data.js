3ddb8e7004019193102d994fe4b5489f89a732a3
Brenda-CW <factotum4rays@gmail.com>
2022-01-07 14:04:24 -0700

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,39 @@
+export const names = [
+    {
+      id: 1,
+      name: "Aster"
+    },
+    {
+      id: 2,
+      name: "Buttercup"
+    },
+    {
+      id: 3,
+      name: "Dahlia"
+    },
+    {
+      id: 4,
+      name: "Lavender"
+    },
+    {
+      id: 5,
+      name: "Poppy"
+    },
+    {
+      id: 6,
+      name: "Iris"
+    },
+    {
+      id: 7,
+      name: "Calendula"
+    },
+    {
+      id: 8,
+      name: "Holly"
+    },
+    {
+      id: 8,
+      name: "Ivy"
+    },
+  ];
+  
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//Lesson: https://learn.skillcrush.com/module-5/write-a-functional-component/
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import React from "react";
+import ReactDOM from "react-dom";
+import NameTag from "./NameTag.js";
+import { names } from "./data.js";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import "./styles.css";
+
+//Callback function that renders a name tag for each name - with instructions to pull the name from the data file
+const renderNameTag = (name) => <NameTag name={name.name} key={name.id} />;
+
+const App = () => {
+  //new array that holds the values of the names data & calls the renderNameTag function
+  const nameTagElements = names.map(renderNameTag);
+  //displays the page heading and triggers the array mapping and sending data to the NameTag component to render each tag
+  return (
+    <div className="App">
+      <h1>Name Tag Generator</h1>
+      {nameTagElements}
+    </div>
+    //WHY CURLY BRACES for nameTagElements???
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
